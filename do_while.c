fe4c2488cf4cdcf7c3562b4732f447e5c804b670
ShobhitKumar1437 <shobhitkumar1437@gmail.com>
2024-08-18 22:13:26 +0530

    Add new file

---
+++ do_while.c
@@ -0,0 +1,37 @@
+ #include<stdio.h>
+int main(){
+       int i=1;
+    do{
+        printf("%d \n",i);
+        i++;
+    } while(i<=5);
+    return 0;
+ }
+
+// Print the sum of first n natural numbers.
+
+#include<stdio.h>
+int main(){
+       int i=1;
+    do{
+        printf("%d \n",i);
+        i++;
+    } while(i<=5);
+    return 0;
+}
+
+// Star pattern
+#include <stdio.h>  
+int main() {  
+int i=1;  
+do             
+{  
+    int j=1;  
+    do{
+      printf("* ");  
+      j++;  
+   }while(j<=5);  
+    printf("\n");  
+    i++;  
+     }while(i<=4);  
+}  
